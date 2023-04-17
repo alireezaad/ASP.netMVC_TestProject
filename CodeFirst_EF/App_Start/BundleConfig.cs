975c91ec7e8bc48d5debfc8eed89bc6efe54137e
Alireza abasi <alireezaad@gmail.com>
2023-04-17 16:41:31 +0430

    fix: fix adding sctipts to bundle in BundleConfig file
    bug was occurding after opening panel page where scripts were calling.

--- CodeFirst_EF/App_Start/BundleConfig.cs
+++ CodeFirst_EF/App_Start/BundleConfig.cs
@@ -5,26 +5,26 @@ namespace CodeFirst_EF
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                     "~/Content/bootstrap-grid.min.css",
+                     "~/Content/bootstrap-rtl.min.css",
+                     "~/Content/bootstrap.min.css",
+                     "~/Content/site.css"));
+
+            
+            bundles.Add(new Bundle("~/Scripts/jquery").Include("~/Scripts/bootstrap.min.js",
+                      "~/Scripts/bootstrap.bundle.js",
+                      "~/Scripts/modernizr-2.8.3.js"));
+
+            // مدل زیر ارور instance of object  میده
+            /*bundles.Add(new ScriptBundle("~/Scripts/jquery").Include(
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/bootstrap.bundle.js",
+                      "~/Scripts/modernizr-2.8.3.js"
+                      ));
+           */
         }
     }
 }
